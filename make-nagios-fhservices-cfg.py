e779ee711ceb759ddd3ffb7c9c8ce3ac3ca0404c
Michael Nairn <mnairn@redhat.com>
2016-07-13 10:07:20 +0100

    Removed all references to 'mbaas', replaced with a more generic 'rhmap' where appropriate.

--- make-nagios-fhservices-cfg.py
+++ make-nagios-fhservices-cfg.py
@@ -4,11 +4,11 @@ import os
 
 from jinja2 import Environment, FileSystemLoader, Template
 
-fh_services_ping = ['fh-mbaas', 'fh-messaging', 'fh-metrics', 'fh-statsd']
-fh_services_health = ['fh-mbaas', 'fh-messaging', 'fh-metrics']
+fh_services_ping = ['fh-messaging', 'fh-metrics']
+fh_services_health = ['fh-messaging', 'fh-metrics']
 
-mbaas_admin_email = os.getenv('MBAAS_ADMIN_EMAIL', 'root@localhost')
-mbaas_router_dns = os.getenv('MBAAS_ROUTER_DNS', 'mbaas.localhost')
+rhmap_admin_email = os.getenv('RHMAP_ADMIN_EMAIL', 'root@localhost')
+rhmap_router_dns = os.getenv('RHMAP_ROUTER_DNS', 'rhmap.localhost')
 
 template_file = '/opt/rhmap/fhservices.cfg.j2'
 nagios_config_filename = '/etc/nagios/conf.d/fhservices.cfg'
@@ -21,8 +21,8 @@ j2template = j2env.get_template(template_basename)
 
 j2renderedouput = j2template.render(fh_services_ping = fh_services_ping,
                                     fh_services_health = fh_services_health,
-                                    mbaas_router_dns=mbaas_router_dns,
-                                    mbaas_admin_email=mbaas_admin_email)
+                                    rhmap_router_dns=rhmap_router_dns,
+                                    rhmap_admin_email=rhmap_admin_email)
 
 with open(nagios_config_filename, 'wb') as nagios_config_file:
     nagios_config_file.write(j2renderedouput)
