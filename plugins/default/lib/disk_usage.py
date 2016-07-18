067ad69e609767ce134d850c879ad0837a86197b
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-07-18 17:50:09 +0200

    WIP disk check in Python
    The idea is to build some abstractions that can be reused in multiple
    checks.

---
+++ plugins/default/lib/disk_usage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import re
+import sys
+from subprocess import CalledProcessError
+
+from openshift import oc
+import nagios
+
+
+check_disk_cmd = ("df", "--output=target,pcent,ipcent")
+# Example output:
+# /etc/hosts      19%    8%
+check_disk_output_pattern = re.compile(r"([\w/]+)\s+(\d+)%\s+(\d+)%")
+
+
+def parse_df(line):
+    mo = check_disk_output_pattern.match(line)
+    if mo is None:
+        return ()
+    return (mo.group(1), int(mo.group(2)), int(mo.group(3)))
+
+def analize(pcent, ipcent, warning_threshold, critical_threshold):
+    max_pcent = max(pcent, ipcent)
+    return max_pcent >= warning_threshold, max_pcent >= critical_threshold
+
+def report():
+    raise NotImplementedError("report is not implemented yet")
+
+def main():
+    project = oc("project", "-q")
+    print analize(map(parse_df, exec_in_pods(project, get_running_pod_names(), check_disk_cmd)))
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except CalledProcessError:
+        pass
+    except KeyError:
+        pass
+    except:
+        sys.exit(nagios.UNKNOWN)
+
+# TODO: check that critical threshold is always >= warning threshold
+# TODO: what if nothing is checked? (empty output from df, etc)
---
+++ plugins/default/lib/openshift.py
@@ -0,0 +1,22 @@
+import json
+from subprocess import check_output
+
+
+def oc(*args):
+    return check_output(("oc",) + args)
+
+def _get_running_pod_names(oc, project):
+    # Manually filter Running pods because of a bug in `oc get`, see:
+    # https://github.com/kubernetes/kubernetes/issues/29115
+    #return oc("-n", project, "get", "pods", "--show-all=false", "-o", "name")
+    pods = json.loads(oc("-n", project, "get", "pods", "--show-all=false", "-o", "json"))
+    return [p["metadata"]["name"] for p in pods if p["status"]["phase"] == "Running"]
+
+def get_running_pod_names(project):
+    return _get_running_pods(oc, project)
+
+def _exec_in_pods(oc, project, pods, cmd):
+    return [oc("-n", project, "exec", name, "--", *cmd) for name in pods]
+
+def exec_in_pods(project, pods, cmd):
+    return _exec_in_all_runnning_pods(oc, project, pods, cmd)
